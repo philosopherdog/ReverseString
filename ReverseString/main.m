7f802f2475f9c6a1b115f0f531f8267ea7d1ab4c
Steven Thompson <steven.thompson.ca@gmail.com>
2016-03-25 16:42:56 -0400

    Initial Commit

---
+++ ReverseString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ReverseString
+//
+//  Created by steve on 2016-03-25.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
